a47e0571a1ad044bb87d65935cd1feca3a90c4d1
Fernando <ferlr33@outlook.com>
2026-03-22 17:50:32 -0600

    Dockerfile, docker-compose y main.py adaptados para despliegue

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,8 @@ RUN pip install --no-cache-dir --upgrade pip \
 
 COPY ./app ./app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+RUN mkdir -p /app/uploads
+
+EXPOSE 8000
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+
 from app.core.config import settings
 from app.utils.files import ensure_upload_directories
 from app.api.router import api_router
 
 app = FastAPI(title="Noona's FastAPI Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://localhost:5500"
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 ensure_upload_directories()
+
 app.mount("/media", StaticFiles(directory=settings.UPLOAD_DIR), name="media")
 app.include_router(api_router)
